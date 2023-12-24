e69b3acc57964d7a2987043187faa240e826d9c2
Matheus <matheusdebrito81@gmail.com>
2023-12-23 23:52:09 -0300

    Modificado arquivos main.js e index.html

--- Dio-Pokedex/assets/js/main.js
+++ Dio-Pokedex/assets/js/main.js
@@ -2,7 +2,37 @@ const offset = 0;
 const limit = 0;
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
+let convertPokemonToLi = (pokemon) => {
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+}
+
+const pokemonList = document.querySelector("#pokemonList")
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        
+        for (let i = 0; i < pokemons.length; i++){
+            const pokemon = pokemons[i];
+            console.log(convertPokemonToLi(pokemon))
+            
+            
+            console.log(pokemonList)
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        } 
+    })
     .catch((error) => console.error(error))
