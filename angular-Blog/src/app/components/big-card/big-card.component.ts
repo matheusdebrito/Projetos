bd832f9fd80e6d6541462a6835c8be9cda2ea1e6
Matheus <matheusdebrito81@gmail.com>
2024-01-15 21:17:56 -0300

    Contéudos desacoplados

--- angular-Blog/src/app/components/big-card/big-card.component.ts
+++ angular-Blog/src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrls: ['./big-card.component.css', './big-card.component.responsive.css']
 })
 export class BigCardComponent {
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = "TITULO"
+  @Input()
+  cardDescription:string = "DESCRIÇÃO"
 
+  constructor(){}
 }
